77ea2a96b3d63c49e4e85f098902b53901b537c5
ashwini_pawar <ashu1472002@gmail.com>
2021-05-03 14:44:29 +0530

    changes on header

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +11,6 @@
     </link>
     <script src="./js/index.js"></script>
 </head>
-
 <body>
 <header> 
       <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
