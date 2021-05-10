911818a820bcb4c102e058257d01839eb1e93115
kalpak nemade <kalpaknemade3a@gmail.com>
2021-05-10 10:30:35 +0530

    dashboard done

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,3 +1,38 @@
+<?php
+session_start();
+
+if (isset($_SESSION['id'])) {
+    include "../database.php";
+    $query = "SELECT * FROM student";
+    $result = mysqli_query($conn, $query);
+    $student =mysqli_num_rows($result);
+    $query = "SELECT * FROM faculty";
+    $result = mysqli_query($conn, $query);
+    $faculty =mysqli_num_rows($result);
+    $query = "SELECT * FROM question Where id=1";
+    $result = mysqli_query($conn, $query);
+    $rating=0;
+    $temp=[];
+    $que=[];
+    if(mysqli_num_rows($result)>0){
+        $que = mysqli_fetch_row($result);
+    }
+    $query = "SELECT noentry FROM rating where `que`= '$que[1]'";
+    
+    $result = mysqli_query($conn, $query);
+    if($result){
+        while ($row = mysqli_fetch_row($result)) {
+            array_push($temp, $row);
+        }
+    }
+    for ($i = 0; $i < sizeof($temp); $i++){
+        $rating+= $temp[$i][0];
+    }
+ 
+} else {
+    echo "<script>alert('You are not login');document.location='../student_login.php'; </script>";
+}
+?>
 <?php include "./header.php"; ?>
             <!--End of Navbar-->
             <!--content section-->
@@ -14,17 +49,18 @@
                     <center>
                     <div class="container-fluid">
                         <div class="row ml-4 mt-2 mb-4 ">
-                            <div class="card card-body  card_cs col-lg-3 mr-4 stat bg-success">
-                                <h3 class="font-weight-bold">800</h3>
+                            <div class="card card-body  card_cs col-lg-3 mr-4 stat bg-success"><center>
+                                <h3 class="font-weight-bold text-center"><?php echo $student ?></h3>
                                 <h5>Total number of students</h5>
+                                </center>
                             </div>
-                            <div class="card card-body card_cs col-lg-3 mr-4 bg-info">
-                                <h3 class="font-weight-bold">200</h3>
-                                <h4>Total number of Faculty members</h4>
+                            <div class="card card-body card_cs col-lg-3 mr-4 bg-info"><center>
+                                <h3 class="font-weight-bold text-center"><?php echo $faculty ?></h3>
+                                <h4>Total number of Faculty members</h4></center>
                             </div>
-                            <div class="card card-body card_cs col-lg-3 mr-4 bg-danger">
-                                <h3 class="font-weight-bold">700</h3>
-                                <h4>Total Feedbacks</h4>
+                            <div class="card card-body card_cs col-lg-3 mr-4 bg-danger"><center>
+                                <h3 class="font-weight-bold text-center"><?php  echo $rating; ?></h3>
+                                <h4>Total Feedbacks</h4></center>
                             </div>  
                         </div>
                     </div>
